f47172c80ac2492ff2ca1c0ec486c409a7c07454
mani254 <manifreelancer25@gmail.com>
2024-01-25 06:39:20 +0530

    in This commit intial index.js file setup along with routes folder is done

---
+++ index.js
@@ -0,0 +1,42 @@
+const express=require('express');
+const mongoose=require('mongoose');
+const dotenv=require('dotenv')
+const cors=require('cors')
+const bodyparser=require('bodyparser')
+//folder imports 
+
+const app=express();
+dotenv.config()
+
+mongoose.connect('mongodb+srv://manikanta:manikanta123@cluster0.0t91iex.mongodb.net/?retryWrites=true&w=majority')
+.then(()=>{
+    console.log('connected to Database successfully')
+})
+.catch((err)=>{
+    console.log(err.message)
+});
+
+//middleware
+app.use(cors({credentials:true,origin:'http://localhost:3000/pos-backend'}))
+app.use(bodyparser.urlencoded({ extended: true }))
+
+
+
+
+// ***** routes ******
+
+app.use('/authentication',require('./routes/authenticationRoutes.js'))
+app.use('/product',require('./routes/productRoutes.js'))
+app.use('/staff',require('./routes/staffRoutes.js'))
+app.use('/producttemplate',require('./routes/productTemplateRoutes.js'))
+app.use('/order',require('./routes/orderRoutes.js'))
+app.use('/branch',require('/routes/branchRoutes.js'))
+
+
+
+
+//listening to port
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>{console.log(`listening on port ${PORT}`)})
